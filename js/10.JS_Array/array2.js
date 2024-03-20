2f5d1ac79b25e63a565b9aeceb117359de3e229e
heesanggod <sangin583@gmail.com>
2024-03-20 12:34:04 +0900

    24.03.20 오전 수업

--- js/10.JS_Array/array2.js
+++ js/10.JS_Array/array2.js
@@ -1,56 +1,102 @@
 /*
     배열 고차 함수
-    - 
+    - 배열의 불변성을 최대한 보장하여 부수 효과(원본에 미친 메서드)를 제거한 메서드
 */
 const array = [7, 11, 20, 1, 8, 3, 16, 2, 19, 9, 10, 4, 13, 12, 14, 17, 15, 5, 18, 6];
 
 /*
-    sort(비교 함수) : 
-    - 
+    sort(비교 함수) : 배열의 요소를 적절하게 정렬
+    - 단순.sort()로 정렬하면 사전식 정렬(앞자리만 보고)을 하게 됨
 */
+// array.sort();
+// console.log(array);
+
+//? 오름차순
+const asc = function (x, y) {
+    return x - y;
+}
+array.sort(asc);
+console.log(array);
+//? 내림차순
+array.sort(function (x,y) {return y - x});
+console.log(array);
+
+// =================================//
+
+const objects = [
+    { x: 9 , y: -5 },
+    { x: -2 , y: 0 },
+    { x: 0 , y: -3 },
+    { x: 6 , y: 5 },
+];
+console.log(objects);
+//? x 를 기준으로 오름차순 정렬하기
+objects.sort(function (a,b) {
+    return a.x - b.x;
+});
+console.log(objects);
 
 
 console.log('==============================');
 
 /*
-    forEach(콜백 함수) : 
-    - 
-    - 
-    - 
-    - 
+    forEach(콜백 함수) : for 문 대신 사용
+    - 배열을 순회하면서 각 요소와 인덱스를 사용할 수 있도록 함 
+    - 원본 배열 변경 X
+    - break, continue, ... 부가적인 작업을 추가 X
+    - for 문보다 성능이 떨어짐 / 가독성이 좋고, 편리하므로 자주 사용됨
+    - async await 작업은 forEach 문으로 사용 불가
 */
-
+array.forEach(function (item, index) {
+    console.log(`index : ${index} , item : ${item}`);
+});
 
 console.log('==============================');
 
 /*
-    map(콜백 함수) : 
-    - 
-    - 
+    map(콜백 함수) : 콜백 함수의 반환값들로 이루어진 새로운 배열 생성해서 반환
+    - 원본 배열 변경 X
+    - forEach : 단순히 요소를 가지고 작업만 진행(반환 값 X)
+        map : 요소를 가지고 작업만 결과로 배열을 새로 만듦(반환 값 O)
 */
-
+const mapArray = array.map(function (item, index) {
+    console.log(`index : ${index} , item : ${item}`);
+    return item * item;
+});
+console.log(mapArray);
+console.log(array);
 
 console.log('==============================');
 
 /*
-    filter(콜백 함수) : 
-    - 
-    - 
+    filter(콜백 함수) : 콜백 함수의 반환 값이 true 인 배열의 요소의 값만을 추출한 새로운 배열을 생성
+    - 배열에서 특정 케이스만 필터링 조건으로 추출하여 새로운 배열을 만들고 싶을 때 사용
+    - 원본 배열 변경 불가능
 */
-
+const filterArray = array.filter(function (item, index) {
+    return item % 2 === 1;
+});
+console.log(filterArray);
+console.log(array);
 
 console.log('==============================');
 
 /*
-    find(value, index) : 
+    find(콜백 함수) : 콜백 함수 결과가 true 인 첫번째 요소를 반환 
 */
-
+const findResult = array.find(function (item, index) {
+    return item % 2 === 1;
+});
+console.log(findResult);
 
 console.log('==============================');
 
 /*
-    find(value, index) : 
+    findIndex(콜백 함수) : 콜백 함수 결과가 true 인 첫번째 요소의 인덱스를 반환
 */
-
+const findIndexResult = array.findIndex(function (item, index) {
+    return item % 2 === 1;
+});
+console.log(findIndexResult);
 
 console.log('==============================');
--- js/9.JS_Function/function.js
+++ js/9.JS_Function/function.js
@@ -1,32 +1,53 @@
 /*
-    함수 : 
-    - 
-    - 
-    - 
-    - 
+    함수 : 어떤 작업을 수행하기 위해 필요한 구문들의 집합
+    - function 함수명 (매개변수, ...) { 실행 구문 }
+    - 동일한 코드를 반복해서 작성할 필요 없이 미리 하나의 코드만 작성한 후 호출하여 사용하여 코드의 재사용성을 높일 수 있음
+    - 특정 기능이나 코드 구문에 대해서 이름을 부여하는 용도로 사용할 수 있음
+    - 특정 작업들에 대해서 모듈화를 위한 용도로 사용할 수 있음
 */
-
+function add (x ,y) {
+    return x + y;
+}
 
 
 /*
-    함수 표현식 : 
+    함수 표현식 : 함수도 객체 리터럴 방식으로 정의 할 수 있고 변수에 할당할 수 있음 
 */
 /*
-    - 
+    - 기명 함수 표현식
 */
+let func1 = add;    //?  함수 할당 값을 더한다는 의미 / 밑의 func2 와 동일식
+// func1 = add();              //? 함수의 결과를 집어넣겠다
 
+const func2 = function add2 (x, y) {
+    return x + y;
+}                               // func1 에 함수가 담겼다
+
+console.log(func1(1,2));
+console.log(func2(1,2));
+//* 기명 함수 형태로 작성한 함수 표현식은 해당 함수명으로 호출 불가능
+// console.log(add2(1, 2));
+
+/*
+    - 익명 함수 표현식
+*/
+const func3 = function (x, y) {
+    return x + y;
+}
+func3(1,2);
 
 
 /*
-    - 
+    콜백 함수 : 다른 함수에 매개변수로 전달하는 함수
 */
+function getlnterger (number) {
+    return number - (number % 1);
+}
 
+function add2 (preProcessing, x, y) {
+    return preProcessing(x) + preProcessing(y);
+}
 
-
-/*
-    콜백 함수 : 
-*/
-
-
-
+const result = add2(getlnterger, 14.67, 16.33);
+console.log(result);
 
